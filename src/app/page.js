bd1702fe6bc36e2ea1e3431fac5ae74c3b80fdb0
jocunda <silviawanshan88@gmail.com>
2023-10-22 17:44:23 +0800

    Feat:add mesh

--- src/app/page.js
+++ src/app/page.js
@@ -5,18 +5,31 @@ import { Canvas } from "@react-three/fiber";
 import { ExperienceRPM } from "@/components/ExperienceRPM.jsx";
 import { ScrollControls, Scroll } from "@react-three/drei";
 import { Interface } from "@/components/Interface";
+import { MotionConfig } from "framer-motion";
 
 export default function Home() {
   return (
     <main className="w-screen h-screen flex flex-col items-center justify-between">
-      <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
-        <ScrollControls pages={2} damping={0.1}>
-          <ExperienceRPM />
-          <Scroll html>
-            <Interface />
-          </Scroll>
-        </ScrollControls>
-      </Canvas>
+      <MotionConfig
+        transition={{
+          type: "spring",
+          mass: 5,
+          stiffness: 500,
+          damping: 50,
+          restDelta: 0.0001,
+        }}
+      >
+        <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
+          <ScrollControls pages={2} damping={0.1}>
+            <Scroll>
+              <ExperienceRPM />
+            </Scroll>
+            <Scroll html>
+              <Interface />
+            </Scroll>
+          </ScrollControls>
+        </Canvas>
+      </MotionConfig>
     </main>
   );
 }
--- src/components/ExperienceRPM.jsx
+++ src/components/ExperienceRPM.jsx
@@ -1,8 +1,15 @@
 import {
   Environment,
   Sky,
+  ambientLight,
+  Float,
+  MeshDistortMaterial,
+  MeshWobbleMaterial,
 } from "@react-three/drei";
+import { motion } from "framer-motion"
+
 import { AvatarRPM } from "./AvatarRPM"
+
 
 export const ExperienceRPM = () => {
 
@@ -10,10 +17,52 @@ export const ExperienceRPM = () => {
     <>
       <Sky />
       <Environment preset="sunset"/>
-      <group position-y={-1}>
-        <AvatarRPM  />
-      </group>
-     
+      <motion.group
+        position={[0.6, -1.5, -1]}
+        scale={1.2}
+        rotation-y={-Math.PI / 10}
+        animate={{
+          y: 0,
+        }}
+      >
+        <AvatarRPM />
+      </motion.group>
+      <motion.group
+        position={[-5, -10, 2]}
+        scale={4}
+        rotation-y={-Math.PI / 10}
+        animate={{
+          z:-10,
+          y:-1.5,
+        }}>
+        <Float>
+          <mesh scale={[1.4, 1.4, 1.4]} position={[-2, -2, -9]}>
+            <sphereGeometry />
+            <MeshDistortMaterial
+              opacity={0.9}
+              transparent
+              distort={0.8}
+              speed={2}
+              color="pink"
+            />
+          </mesh>
+        </Float>
+        <Float>
+          <mesh position={[-2, -5, -15]} scale={[2, 2, 2]}>
+            <sphereGeometry />
+            <MeshDistortMaterial
+              opacity={0.4}
+              transparent
+              distort={0.4}
+              speed={4}
+              color="lime"
+            />
+          </mesh>
+        </Float>
+        <directionalLight position={[-5, 3, 5]} intensity={0.4} />
+      </motion.group>
+      <ambientLight intensity={0.3} />
+      
     </>
   );
 };
